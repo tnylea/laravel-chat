7ae8854599e27d7752240da9341398052800d6b5
Tony Lea <tony@devdojo.com>
2025-04-14 12:28:23 -0400

    Adding updates to chat

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -7,15 +7,9 @@ import { initializeTheme } from './hooks/use-appearance';
 import { configureEcho } from '@/hooks/use-echo';
 
 configureEcho({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY || 'shoutbox_key',
-    wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1',
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME || 'http') === 'https',
-    enabledTransports: ['ws', 'wss'],
-    disableStats: true,
-    cluster: 'mt1',
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     authEndpoint: '/broadcasting/auth',
 });
 
